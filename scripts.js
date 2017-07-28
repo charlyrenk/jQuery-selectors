2fe53321c1cafbfe1bd3cce4d9b274dee57a9409
Charles Renk <renkx008@umn.edu>
2017-07-28 13:06:12 -0500

    second comment

---
+++ scripts.js
@@ -0,0 +1,11 @@
+console.log("The Script is Sourced!")
+$(document).ready(function(){
+    $('body').append('<h2>Hey there!</h2>');
+    $('body').prepend("<h2>I am going to the top</h2>");
+    $('li').css('color','blue');
+    $('#kevin li').css('color','maroon')
+    $('.food').css('color','pink');
+    $('#chief').css('color','green');
+    $('li')
+    
+});
