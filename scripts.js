68b1221893fc8c807082ec1da81f5d01b749b4e9
Charles Renk <renkx008@umn.edu>
2017-07-28 14:21:04 -0500

    added counter to button

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 console.log("The Script is Sourced!")
+var clicks = 0
+// //function clickNumber(){
+//     clicks += 1;
+// }
 $(document).ready(function(){
     $('body').append('<h2>Hey there!</h2>');
     $('body').prepend("<h2>I am going to the top</h2>");
@@ -8,6 +12,10 @@ $(document).ready(function(){
     $('#chief').css('color','green');
     $('ul').first().children().last().css('color','black');
     $('#kevinButton').on('click', function(){
-        console.log('button was clicked');
-    });
+        //clickNumber();
+        clicks++;
+        console.log('button was clicked', clicks, 'times.') ;
+        
+
+    })
 });
